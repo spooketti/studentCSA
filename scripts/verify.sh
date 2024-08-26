8277860a64bc4ef6add1bf691e3ddd49617bc893
spooketti <jonliu168@gmail.com>
2024-08-26 09:37:28 -0700

    redoing this

---
+++ scripts/verify.sh
@@ -0,0 +1,95 @@
+ruby -v
+bundle -v
+python --version
+jupyter --version
+git config --global --list
+
+cat <<EOF > /tmp/variables.sh
+export project_dir=$HOME/studentCSA  # change nighthawk to different name to test your git clone
+export project=\$project_dir/studentCSA  # change portfolio_2025 to name of project from git clone
+export project_repo="https://github.com/spooketti/studentCSA.git"  # change to project you created from portfolio_2025 template 
+EOF
+
+echo "Project dir: $project_dir"
+echo "Project: $project"
+echo "Repo: $project_repo"
+
+# Extract saved variables
+source /tmp/variables.sh
+
+echo "Using conditional statement to create a project directory and project"
+
+cd ~    # start in home directory
+
+# Conditional block to make a project directory
+if [ ! -d $project_dir ]
+then 
+    echo "Directory $project_dir does not exist... making directory $project_dir"
+    mkdir -p $project_dir
+fi
+echo "Directory $project_dir exists." 
+
+# Conditional block to git clone a project from project_repo
+if [ ! -d $project ]
+then
+    echo "Directory $project does not exist... cloning $project_repo"
+    cd $project_dir
+    git clone $project_repo
+    cd ~
+fi
+echo "Directory $project exists."
+echo "Navigate to project, then navigate to area wwhere files were cloned"
+cd $project
+pwd
+
+echo ""
+echo "list top level or root of files with project pulled from github"
+ls
+echo "Navigate to project, then navigate to area wwhere files were cloned"
+cd $project
+pwd
+
+echo ""
+echo "list all files in long format"
+ls -al
+
+echo "Look for posts"
+export posts=$project/_posts  # _posts inside project
+cd $posts  # this should exist per fastpages
+pwd  # present working directory
+ls -lR
+
+echo "Look for notebooks"
+export notebooks=$project/_notebooks  # _notebooks is inside project
+cd $notebooks   # this should exist per fastpages
+pwd  # present working directory
+ls -lR 
+
+echo "Look for images, print working directory, list files"
+cd $project/images  # this should exist per fastpages
+pwd
+ls -lR
+echo "Navigate to project, then navigate to area wwhere files were cloned"
+
+cd $project
+echo "show the contents of README.md"
+echo ""
+
+cat README.md  # show contents of file, in this case markdown
+echo ""
+echo "end of README.md"
+echo "Show the shell environment variables, key on left of equal value on right"
+echo ""
+
+env
+
+cd $project
+
+echo ""
+echo "show the secrets of .git config file"
+cd .git
+ls -l config
+
+echo ""
+echo "look at config file"
+cat config
