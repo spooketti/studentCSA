f92b22d6db2e1eff352c9c9adf6a68ac179d368f
spooketti <jonliu168@gmail.com>
2024-08-28 10:14:34 -0700

    tool memories

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,8 +5,8 @@ jupyter --version
 git config --global --list
 
 cat <<EOF > /tmp/variables.sh
-export project_dir=$HOME/studentCSA  # change nighthawk to different name to test your git clone
-export project=\$project_dir/studentCSA  # change portfolio_2025 to name of project from git clone
+export project_dir=$HOME/testStudentCSA  # change nighthawk to different name to test your git clone
+export project=\$project_dir/testStudentCSA  # change portfolio_2025 to name of project from git clone
 export project_repo="https://github.com/spooketti/studentCSA.git"  # change to project you created from portfolio_2025 template 
 EOF
 
